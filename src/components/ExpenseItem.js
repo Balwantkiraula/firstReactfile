095d267d939ae63439740094d4c189203dde8b29
Balwantkiraula <balwantkiraula123@gmail.com>
2023-03-15 13:33:03 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <div></div>
+            <div>
+            <h2>Food Rs 10</h2>
+            <h2>Petrol Rs 100</h2>
+            <h2>Movies Rs 200</h2>
+            <div></div>
+            </div>
+
+        </div>
+    )
+    
+}
+export default ExpenseItem;
