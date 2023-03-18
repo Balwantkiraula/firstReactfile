095b355e9411cc572c498206d52ac8f51b900cc3
Balwantkiraula <balwantkiraula123@gmail.com>
2023-03-18 12:41:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from './components/Expenses/ExpenseItem';
 function App() {
   const expenses=[ 
     {title: 'Car Insurance', amount: 256.45, date: new Date(2023, 3, 16)}, 
   {title: 'Car', amount: 25687.45, date: new Date(2023, 3, 6)}, 
-  {title: 'Insurance', amount: 2596.45, date: new Date(2023, 3, 19)}, 
-  {title: 'Car In', amount: 2560.45, date: new Date(2013, 3, 16)}, 
+  {title: 'Bike Insurance', amount: 296.45, date: new Date(2023, 1, 19)}, 
+  {title: 'Bike', amount: 22560.45, date: new Date(2013, 2, 16)}, 
 ];
   return (
     <div >
